743133de02382de0aef563caf0445d1ad8d7a6b6
sq1533 <618356@naver.com>
2024-09-25 10:00:42 +0900

    feat: 인증번호 입력시간 초과 실패step 추가

--- worksMail.py
+++ worksMail.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 
 def ezMail(id:str,pw:str,botAPI:str,botID:str):
+    mailReset = {"passnumber":"test","addr":"test_a","subaddr":"test_s","title":"test_t","main":"test_m"}
     #크롬 드라이버 옵션 설정
     from selenium import webdriver
     from selenium.webdriver.common.action_chains import ActionChains
@@ -37,78 +38,78 @@ def ezMail(id:str,pw:str,botAPI:str,botID:str):
     passingMail = driver.find_element(By.XPATH,'//a[@id="privateEmailButton"]')
     ActionChains(driver).click(passingMail).perform()
     time.sleep(5)
+    exit_flag = False
     while True:
         mail = pd.read_json("C:\\Users\\USER\\ve_1\\DB\\4-3mailAccess.json",orient='records',dtype={"passnumber":str,"addr":str,"subaddr":str,"title":str,"main":str})
         #인증번호 검증
-        if mail['passnumber'].tolist()[-1].isdigit():
-            #인증 진행
-            passingN = driver.find_element(By.XPATH,'//input[@id="number1"]')
+        for i in range(300):
             passingnumber = mail['passnumber'].tolist()[-1]
-            ActionChains(driver).click(passingN).send_keys('{}'.format(passingnumber)).perform()
-            time.sleep(1)
-            #메일전송 페이지 전환
-            driver.get('https://mail.worksmobile.com/#/compose?orderType=new')
-            time.sleep(2)
-            driver.maximize_window()
-            time.sleep(1)
-            #로그인 검증
-            try:
-                address = driver.find_element(By.XPATH,'//input[@aria-label="받는사람"]')#수신자 입력창
-                subaddress = driver.find_element(By.XPATH,'//input[@aria-label="참조"]')#참조 입력창
-                mailtitle = driver.find_element(By.XPATH,'//input[@aria-label="제목"]')#제목 입력창
-                mailmain = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[3]/div[2]/div/fieldset/div/div/div/div[3]')#내용 입력창
-                send_button = driver.find_element(By.XPATH,'//button[@data-hotkey="sendKey"]')#전송 버튼
-                send_each = driver.find_element(By.XPATH,'//input[@name="sendSeparately"]')#개인별 체크박스
-                #메일내용 입력
-                mmain = mail['main'].tolist()[-1]
-                ActionChains(driver).click(mailmain).send_keys(Keys.PAGE_UP).send_keys('{}'.format(mmain)).perform()
+            if passingnumber.isdigit():
+                #인증 진행
+                passingN = driver.find_element(By.XPATH,'//input[@id="number1"]')
+                ActionChains(driver).click(passingN).send_keys('{}'.format(passingnumber)).perform()
                 time.sleep(1)
-                #메일제목 입력
-                mtitle = mail['title'].tolist()[-1]
-                ActionChains(driver).click(mailtitle).send_keys_to_element(mailtitle,'{}'.format(mtitle)).perform()
+                #메일전송 페이지 전환
+                driver.get('https://mail.worksmobile.com/#/compose?orderType=new')
+                time.sleep(2)
+                driver.maximize_window()
                 time.sleep(1)
-                #참조 입력
-                subaddrs = mail['subaddr'].tolist()[-1]
-                ActionChains(driver).send_keys_to_element(subaddress, '{}'.format(subaddrs)).perform()
-                time.sleep(1)
-                #수신자 입력
-                addrs = mail['addr'].tolist()[-1]
-                ActionChains(driver).send_keys_to_element(address, '{}'.format(addrs)).perform()
-                time.sleep(1)
-                #개인별 전송 클릭
-                ActionChains(driver).click(send_each).perform()
-                time.sleep(1)
-                #전송 클릭_1
-                ActionChains(driver).click(send_button).perform()
-                time.sleep(5)
-                """
-                #전송 클릭_2
-                send_button = driver.find_element(By.XPATH,'/html/body/div[3]/div/div/div/h3/div/button[1]')
-                ActionChains(driver).click(send_button).perform()
-                time.sleep(5)
-                """
-                driver.quit()
-                mailReset = {
-                    "passnumber":"test",
-                    "addr":"test_a",
-                    "subaddr":"test_s",
-                    "title":"test_t",
-                    "main":"test_m"
-                    }
-                pd.DataFrame(mailReset,index=[0]).to_json('C:\\Users\\USER\\ve_1\\DB\\4-3mailAccess.json',orient='records',force_ascii=False,indent=4)
-                requests.get(f"https://api.telegram.org/bot{botAPI}/sendMessage?chat_id={botID}&text=메일전송 성공")
-                time.sleep(1)
-                break
-            except NoSuchElementException:
-                mailReset = {"passnumber":"test","addr":"test_a","subaddr":"test_s","title":"test_t","main":"test_m"}
-                pd.DataFrame(mailReset,index=[0]).to_json('C:\\Users\\USER\\ve_1\\DB\\4-3mailAccess.json',orient='records',force_ascii=False,indent=4)
-                requests.get(f"https://api.telegram.org/bot{botAPI}/sendMessage?chat_id={botID}&text=인증실패, 재시도 요망")
-                driver.quit()
-                time.sleep(1)
-                break
-        else:
-            time.sleep(0.5)
-            pass
+                #로그인 검증
+                try:
+                    address = driver.find_element(By.XPATH,'//input[@aria-label="받는사람"]')#수신자 입력창
+                    subaddress = driver.find_element(By.XPATH,'//input[@aria-label="참조"]')#참조 입력창
+                    mailtitle = driver.find_element(By.XPATH,'//input[@aria-label="제목"]')#제목 입력창
+                    mailmain = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[3]/div[2]/div/fieldset/div/div/div/div[3]')#내용 입력창
+                    send_button = driver.find_element(By.XPATH,'//button[@data-hotkey="sendKey"]')#전송 버튼
+                    send_each = driver.find_element(By.XPATH,'//input[@name="sendSeparately"]')#개인별 체크박스
+                    #메일내용 입력
+                    mmain = mail['main'].tolist()[-1]
+                    ActionChains(driver).click(mailmain).send_keys(Keys.PAGE_UP).send_keys('{}'.format(mmain)).perform()
+                    time.sleep(1)
+                    #메일제목 입력
+                    mtitle = mail['title'].tolist()[-1]
+                    ActionChains(driver).click(mailtitle).send_keys_to_element(mailtitle,'{}'.format(mtitle)).perform()
+                    time.sleep(1)
+                    #참조 입력
+                    subaddrs = mail['subaddr'].tolist()[-1]
+                    ActionChains(driver).send_keys_to_element(subaddress, '{}'.format(subaddrs)).perform()
+                    time.sleep(1)
+                    #수신자 입력
+                    addrs = mail['addr'].tolist()[-1]
+                    ActionChains(driver).send_keys_to_element(address, '{}'.format(addrs)).perform()
+                    time.sleep(1)
+                    #개인별 전송 클릭
+                    ActionChains(driver).click(send_each).perform()
+                    time.sleep(1)
+                    #전송 클릭_1
+                    ActionChains(driver).click(send_button).perform()
+                    time.sleep(5)
+                    """
+                    #전송 클릭_2
+                    send_button = driver.find_element(By.XPATH,'/html/body/div[3]/div/div/div/h3/div/button[1]')
+                    ActionChains(driver).click(send_button).perform()
+                    time.sleep(5)
+                    """
+                    driver.quit()
+                    pd.DataFrame(mailReset,index=[0]).to_json('C:\\Users\\USER\\ve_1\\DB\\4-3mailAccess.json',orient='records',force_ascii=False,indent=4)
+                    requests.get(f"https://api.telegram.org/bot{botAPI}/sendMessage?chat_id={botID}&text=메일전송 성공")
+                    time.sleep(1)
+                    exit_flag = True
+                    break
+                except NoSuchElementException:
+                    pd.DataFrame(mailReset,index=[0]).to_json('C:\\Users\\USER\\ve_1\\DB\\4-3mailAccess.json',orient='records',force_ascii=False,indent=4)
+                    requests.get(f"https://api.telegram.org/bot{botAPI}/sendMessage?chat_id={botID}&text=인증실패, 재시도 요망")
+                    driver.quit()
+                    time.sleep(1)
+                    exit_flag = True
+                    break
+            else:
+                time.sleep(0.5)
+                pass
+        if exit_flag:break
+        pd.DataFrame(mailReset,index=[0]).to_json('C:\\Users\\USER\\ve_1\\DB\\4-3mailAccess.json',orient='records',force_ascii=False,indent=4)
+        requests.get(f"https://api.telegram.org/bot{botAPI}/sendMessage?chat_id={botID}&text=인증 시간 초과")
+        break
 
 if __name__ == "__main__":
     while True:
