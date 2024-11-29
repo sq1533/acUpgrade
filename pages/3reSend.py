81c6e369e490d9e35fa4948edda969ebf31a6f5b
sq1533 <618356@naver.com>
2024-11-29 12:54:10 +0900

    feat : 주석 추가

--- pages/3reSend.py
+++ pages/3reSend.py
@@ -4,6 +4,7 @@ import streamlit as st
 from customs.custom import css
 #사이드바 제거
 st.markdown(css, unsafe_allow_html=True)
+#미정산 거래 정보기입
 st.header("미정산거래, 팩스 재전송 필요")
 textArea,emptyArea = st.columns([2,1],vertical_alignment="bottom")
 Date = textArea.text_input(label="수신 일시",value=None,placeholder="MM/DD hh:mm",label_visibility="visible")
@@ -14,10 +15,11 @@ Info = textArea.text_input(label="피해자 정보(이름, 금액 등)",value=No
 emptyArea.empty()
 secter1,secter2 = st.columns([2,1],vertical_alignment="bottom")
 signUp = secter1.radio(label="민원등록 여부",options=["등록 완료","등록 미완료(등록 필요)"])
+#저장
 if secter2.button(label="저장"):
     read = pd.read_json("C:\\Users\\USER\\ve_1\\DB\\6-2reMind.json",orient='records',dtype={'date':str,'bank':str,'info':str,'signUp':str})
     new = pd.DataFrame(data={"date":Date,"bank":Bank,"info":Info,"signUp":signUp},index=[0])
     pd.concat([read,new],ignore_index=True).to_json("C:\\Users\\USER\\ve_1\\DB\\6-2reMind.json",orient='records',force_ascii=False,indent=4)
     with st.spinner('전송중....'):
         time.sleep(1)
-        st.success(body="저장완료")
+    st.success(body="저장완료")
