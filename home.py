4b40e1c567b10910478b6dfeb50a48c744cee4ec
sq1533 <618356@naver.com>
2025-02-04 11:58:52 +0900

    feat : 핫라인 위치 변경 및 UI 기능 변경

--- home.py
+++ home.py
@@ -56,8 +56,8 @@ def H_page() -> None:
                     st.success('핫라인을 확인해주세요.')
         coor = []
         unfixed = st.text_input("핫라인 전파")
-        if st.checkbox("카카오페이",value=True):coor.append(0)
-        if st.checkbox("쿠팡",value=True):coor.append(1)
+        if st.checkbox("쿠팡",value=True):coor.append(0)
+        if st.checkbox("카카오페이",value=True):coor.append(1)
         if st.checkbox("카카오모빌리티",value=True):coor.append(2)
         if st.checkbox("네이버페이",value=True):coor.append(3)
         if st.checkbox("카카오 인증서",value=True):coor.append(4)
